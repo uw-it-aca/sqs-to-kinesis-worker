8a6813665a7aea9a3c0344cee84f2b01c229bc93
Ethan Turner <ezturner@uw.edu>
2018-04-04 12:25:37 -0700

    Deleting test

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -3,18 +3,6 @@ import os
 from unittest.mock import patch
 import index
 
-
-class TestHandlerCase(unittest.TestCase):
-
-    def test_response(self):
-        print("testing response.")
-        result = index.handler(None, None)
-        print(result)
-        self.assertEqual(result['statusCode'], 200)
-        self.assertEqual(result['headers']['Content-Type'], 'application/json')
-        self.assertIn('Hello World', result['body'])
-
-
 class TestConfigs(unittest.TestCase):
 
     def test_sqs_url(self):
